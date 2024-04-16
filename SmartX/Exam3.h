ce2e9843cdd2f22db2a7fda16507a1d9c88d5c82
GVEncheva22 <GVEncheva22@codingburgas.bg>
2024-04-17 00:55:15 +0300

    Add exam to the third lesson

--- SmartX/Exam3.h
+++ SmartX/Exam3.h
@@ -34,11 +34,49 @@ namespace SmartX {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::RadioButton^ radioAnswer1;
+	private: System::Windows::Forms::RadioButton^ radioAnswer2;
+	private: System::Windows::Forms::RadioButton^ radioAnswer3;
+	private: System::Windows::Forms::RadioButton^ radioAnswer4;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
+		// Static array of questions
+		static array<System::String^>^ questions = gcnew array<System::String^>{
+			"What does #include <iostream> allow us to do?",
+				"What does using namespace std; do?",
+				"What is the purpose of int main() in C++?",
+				"How is cout typically used in C++?",
+				"What does return 0; signify in the main function?"
+		};
+
+		// Static 2D array of answers for each question
+		static array<array<System::String^>^>^ answers = gcnew array<array<System::String^>^>{
+			gcnew array<System::String^>{"Work with mathematical functions", "Work with input and output objects", "Define custom data types", "Manage memory allocation"},
+				gcnew array<System::String^>{"Imports a custom namespace for variable declarations", "Allows the use of objects and variables from the standard library", "Restricts the scope of variables to the main function", "Defines a new namespace for output operations"},
+				gcnew array<System::String^>{"Declares a custom function for mathematical operations", "Starts the execution of the program", "Defines a class for object-oriented programming", "Represents a placeholder for variable declarations"},
+				gcnew array<System::String^>{"To input data from the user", "To declare variables", "To perform mathematical operations", "To output/print text to the console"},
+				gcnew array<System::String^>{"To output/print text to the console", "Terminates the program abruptly", "Prompts the user for further input", "Starts an infinite loop"}
+		};
+
+		// Static array of indices for the correct answer of each question
+		static array<int>^ correctAnswers = gcnew array<int>{1, 1, 1, 3, 0};
+
+		// Fields for tracking the current question and number of right answers
+		static int currentQuestionIndex = 0;
+		static int rightAnswersCount = 0;
+		static const int questionCount = 5;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary> 
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
@@ -48,12 +86,201 @@ namespace SmartX {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Exam3";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Exam3::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->radioAnswer1 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioAnswer2 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioAnswer3 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioAnswer4 = (gcnew System::Windows::Forms::RadioButton());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(191, 62);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(2);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(238, 168);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 17;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button1->Location = System::Drawing::Point(258, 235);
+			this->button1->Margin = System::Windows::Forms::Padding(2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(84, 37);
+			this->button1->TabIndex = 16;
+			this->button1->Text = L"Отговори";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Exam3::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Lavender;
+			this->button2->Location = System::Drawing::Point(32, 234);
+			this->button2->Margin = System::Windows::Forms::Padding(2);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(95, 40);
+			this->button2->TabIndex = 15;
+			this->button2->Text = L"Откажи се";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Exam3::button2_Click);
+			// 
+			// radioAnswer1
+			// 
+			this->radioAnswer1->AutoSize = true;
+			this->radioAnswer1->Location = System::Drawing::Point(32, 78);
+			this->radioAnswer1->Margin = System::Windows::Forms::Padding(2);
+			this->radioAnswer1->Name = L"radioAnswer1";
+			this->radioAnswer1->Size = System::Drawing::Size(85, 17);
+			this->radioAnswer1->TabIndex = 11;
+			this->radioAnswer1->TabStop = true;
+			this->radioAnswer1->Text = L"radioButton1";
+			this->radioAnswer1->UseVisualStyleBackColor = true;
+			// 
+			// radioAnswer2
+			// 
+			this->radioAnswer2->AutoSize = true;
+			this->radioAnswer2->Location = System::Drawing::Point(32, 115);
+			this->radioAnswer2->Margin = System::Windows::Forms::Padding(2);
+			this->radioAnswer2->Name = L"radioAnswer2";
+			this->radioAnswer2->Size = System::Drawing::Size(85, 17);
+			this->radioAnswer2->TabIndex = 12;
+			this->radioAnswer2->TabStop = true;
+			this->radioAnswer2->Text = L"radioButton1";
+			this->radioAnswer2->UseVisualStyleBackColor = true;
+			// 
+			// radioAnswer3
+			// 
+			this->radioAnswer3->AutoSize = true;
+			this->radioAnswer3->Location = System::Drawing::Point(32, 151);
+			this->radioAnswer3->Margin = System::Windows::Forms::Padding(2);
+			this->radioAnswer3->Name = L"radioAnswer3";
+			this->radioAnswer3->Size = System::Drawing::Size(85, 17);
+			this->radioAnswer3->TabIndex = 13;
+			this->radioAnswer3->TabStop = true;
+			this->radioAnswer3->Text = L"radioButton1";
+			this->radioAnswer3->UseVisualStyleBackColor = true;
+			// 
+			// radioAnswer4
+			// 
+			this->radioAnswer4->AutoSize = true;
+			this->radioAnswer4->Location = System::Drawing::Point(32, 186);
+			this->radioAnswer4->Margin = System::Windows::Forms::Padding(2);
+			this->radioAnswer4->Name = L"radioAnswer4";
+			this->radioAnswer4->Size = System::Drawing::Size(85, 17);
+			this->radioAnswer4->TabIndex = 14;
+			this->radioAnswer4->TabStop = true;
+			this->radioAnswer4->Text = L"radioButton1";
+			this->radioAnswer4->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(29, 20);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"label1";
+			// 
+			// Exam3
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(458, 294);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->radioAnswer4);
+			this->Controls->Add(this->radioAnswer3);
+			this->Controls->Add(this->radioAnswer2);
+			this->Controls->Add(this->radioAnswer1);
+			this->Controls->Add(this->label1);
+			this->Name = L"Exam3";
+			this->Text = L"Exam3";
+			this->Load += gcnew System::EventHandler(this, &Exam3::Exam3_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void Exam3_Load(System::Object^ sender, System::EventArgs^ e) {
+		loadQuestions();
+	}
+			private: System::Void loadQuestions() {
+				// Check last question
+				if (currentQuestionIndex == questionCount) {
+					MessageBox::Show("Right answers: " + rightAnswersCount);
+					if (rightAnswersCount < questionCount) {
+						MessageBox::Show("You do not have enough points to proceed.");
+						rightAnswersCount = 0;
+						currentQuestionIndex = 0;
+					}
+					else {
+						MessageBox::Show("Congratulations, you can now proceed to the next tutorial.");
+						// TODO Show main form!!!
+
+						this->Close();
+						return;
+					}
+				}
+
+				// Set the question
+				label1->Text = questions[currentQuestionIndex];
+
+				// Set the answer choices
+				radioAnswer1->Text = answers[currentQuestionIndex][0];
+				radioAnswer2->Text = answers[currentQuestionIndex][1];
+				radioAnswer3->Text = answers[currentQuestionIndex][2];
+				radioAnswer4->Text = answers[currentQuestionIndex][3];
+
+			}
+
+	private: System::Int32 getAnswer() {
+		if (radioAnswer1->Checked) {
+			return 0;
+		}
+		else if (radioAnswer2->Checked) {
+			return 1;
+		}
+		else if (radioAnswer3->Checked) {
+			return 2;
+		}
+		else if (radioAnswer4->Checked) {
+			return 3;
+		}
+		return -1;
+	}
+
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (getAnswer() == correctAnswers[currentQuestionIndex]) {
+		rightAnswersCount++;
+	}
+
+	currentQuestionIndex++;
+	loadQuestions();
+}
+	   private: void LoadCurrentQuestion() {
+		   label1->Text = questions[currentQuestionIndex];
+		   radioAnswer1->Text = answers[currentQuestionIndex][0];
+		   radioAnswer2->Text = answers[currentQuestionIndex][1];
+		   radioAnswer3->Text = answers[currentQuestionIndex][2];
+		   radioAnswer4->Text = answers[currentQuestionIndex][3];
+	   }
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+};
 }
